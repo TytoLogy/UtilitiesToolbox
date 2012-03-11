522f887db5f511d8258c94107c7c099c89a4919d
Sharad Shanbhag <sshanbhag@neoucom.edu>
2012-03-11 15:56:50 -0400

    modified:   UIUtilities/read_ui_str.m unknown:   .DS_Store

--- UIUtilities/read_ui_str.m
+++ UIUtilities/read_ui_str.m
@@ -1,14 +1,34 @@
 function out = read_ui_str(handle, str)
-%function read_ui_str(handle, str)
+%--------------------------------------------------------------------------
+% read_ui_str(handle, str)
+%--------------------------------------------------------------------------
+% UIUtilities Toolbox
+%--------------------------------------------------------------------------
+%	Reads the UI control string value
 %
-%	Reads the UI control string
 %	if 'str' is set to 'n', string is converted to a number before being
 %	returned.
+%--------------------------------------------------------------------------
+% Input Arguments:
+% 		handle	handle to ui object
+% 		str		'n' returns a number
+% 					default returns string
+% Output Arguments:
+%		out		string or numerical array
 %
+%--------------------------------------------------------------------------
 %	See Also: UPDATE_UI_STR, UPDATE_UI_VAL, READ_UI_VAL
+%--------------------------------------------------------------------------
+
+%------------------------------------------------------------------------
+% Sharad Shanbhag
+% sshanbhag@neomed.edu
+%------------------------------------------------------------------------
+% Created: long long ago
 %
-% Sharad J. Shanbhag
-% sharad@etho.caltech.edu
+% Modified:
+%	11 Mar 2012 (SJS): updated documentation
+%------------------------------------------------------------------------
 
 out = get(handle, 'String');
 
