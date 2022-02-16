97601cae017cecb4e2b369ba67c0a36e74d38584
Sharad Shanbhag <sshanbhag@neomed.edu>
2022-02-16 13:34:12 -0500

    Update replace_file_ext.m

--- GeneralUtilities/replace_file_ext.m
+++ GeneralUtilities/replace_file_ext.m
@@ -1,6 +1,11 @@
 function [new, varargout] = replace_file_ext(orig, new_ext)
 % changes the extension of orig file name
 
+% check that new_ext has '.' as first element
+if new_ext(1) ~= '.'
+   error('%s: first character of new extension must be a . ', mfilename)
+end
+
 % break up orig name into path, file base name, extension
 [pstr, fstr, estr] = fileparts(orig);
 
