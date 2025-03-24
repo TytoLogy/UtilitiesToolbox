78402cfcf0ff4a973c5ae684f375bfb12303a096
Sharad Shanbhag <sshanbhag@neomed.edu>
2025-03-24 19:11:50 -0400

    created larger start, end padding

--- GeneralUtilities/moving_average.m
+++ GeneralUtilities/moving_average.m
@@ -5,13 +5,17 @@ function y = moving_average(x, w)
 k = ones(1, w) ./ w;
 % don't want to use normal 0 padding for x vector.  instead, add copies
 % of 1st and last values to x vector
+
+% pad length is 2*w
+wpad = 3*w;
+
 % first, force x to row vector
 x = x(:)';
 % store original length of x
 xlen = length(x);
 % then, pad vector with w copies of first and last x values
-x = [ x(1)*ones(1, w) x x(end)*ones(1, w)];
+x = [ x(1)*ones(1, wpad) x x(end)*ones(1, wpad)];
 % convolve
 y = conv(x, k, 'same');
 % truncate to proper length
-y = y(w + (1:xlen));
+y = y(wpad + (1:xlen));
